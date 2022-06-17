105cbada96354bc5829b2132b8d69e6477ba3fb9
Aurelien <aurelien.strimelle@bstorm.be>
2022-06-17 13:48:42 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_ASP_MVC_Exo02/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo_ASP_MVC_Exo02.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public decimal Price { get; set; }
+        public int? Discount { get; set; }
+        public string Reference { get; set; }
+    }
+}
